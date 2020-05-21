7356477c8fee5b9bd1ac8cbb1af6748891bcf4ea
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-05-21 17:59:10 +0000

    Navbar adjusted

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,13 +3,31 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light  navbar-expand-lg bg-white mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">
+					<i className="fas fa-fish" /> saleswhale
+				</span>
 			</Link>
+			<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div className="navbar-nav mx-auto">
+					<a className="nav-item nav-link mx-2" href="#">
+						<b>Why SalesWhale?</b>
+					</a>
+					<a className="nav-item nav-link mx-2" href="#">
+						<b>Product</b>
+					</a>
+					<a className="nav-item nav-link mx-2" href="#">
+						<b>Company</b>
+					</a>
+				</div>
+			</div>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-white border border-primary mr-1 rounded-lg">Log In</button>
+				</Link>
+				<Link to="/demo">
+					<button className="btn btn-primary rounded-lg">Get a Demo</button>
 				</Link>
 			</div>
 		</nav>
